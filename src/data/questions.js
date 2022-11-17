91793ed3cf8637ddcfa6afc87ff0896557d18244
Jose Lima <jalimag0206@gmail.com>
2022-11-17 18:09:10 -0300

    add files project

---
+++ src/data/questions.js
@@ -0,0 +1,110 @@
+const data = [
+    {
+      question: "Qual ano foi a primeira copa do mundo ?",
+      options: [
+        "1930",
+        "1934",
+        "1926",
+        "1938",
+      ],
+      answer: "1930",
+    },
+    {
+      question: "Qual o maior artilheiro da copa do mundo ?",
+      options: [
+        "Ronaldo",
+        "Pelé",
+        "Klose",
+        "Maradona"
+      ],
+      answer: "Klose",
+    },
+    {
+      question: "Qual o país com mais cartões vermelhos ? (até a copa de 2018)",
+      options: [
+        "Brasil",
+        "Suiça",
+        "Argentina",
+        "França"
+      ],
+      answer: "Brasil",
+    },
+    {
+      question: "Em qual ano foi utilizado os cartões vermelhos/amarelos pela primeira vez ?",
+      options: [
+        "1962",
+        "1966",
+        "1970",
+        "1958"
+      ],
+      answer: "1966",
+    },
+    {
+      question:
+        "Quanto é a premiação para o vencedor da copa do mundo 2022?",
+      options: [
+        "US$ 50 milhões",
+        "US$ 53 milhões",
+        "US$ 47 milhões",
+        "US$ 42 milhões"
+      ],
+      answer: "US$ 42 milhões",
+    },
+    {
+      question:
+        "Qual país chegou em mais finais de copa do mundo ?",
+      options: [
+        "Itália",
+        "Alemanha",
+        "Brasil",
+        "França"
+      ],
+      answer: "Alemanha",
+    },
+    {
+      question:
+        "Em qual dessas edições tivemos o gol de ouro ?",
+      options: [
+        "1966",
+        "1978",
+        "1990",
+        "1998"
+      ],
+      answer: "1998",
+    },
+    {
+      question:
+        "Qual dessas seleções ja foi eliminada sem ter sofrido um gol ?",
+      options: [
+        "Suiça",
+        "México",
+        "Costa Rica",
+        "Japão"
+      ],
+      answer: "Suiça",
+    },
+    {
+      question:
+        "Em quais anos o Brasil foi campeão mundial ?",
+      options: [
+        "1958, 1962, 1970, 1990 e 2002",
+        "1954, 1962, 1970, 1994 e 2002",
+        "1954, 1958, 1970, 1994 e 2002",
+        "1958, 1962, 1970, 1994 e 2002"
+      ],
+      answer: "1958, 1962, 1970, 1994 e 2002",
+    },
+    {
+      question:
+        "Qual foi a edição de copa do mundo com mais gols contras ?",
+      options: [
+        "2006",
+        "2018",
+        "2002",
+        "2010"
+      ],
+      answer: "2018",
+    },
+  ];
+  
+  export default data;
